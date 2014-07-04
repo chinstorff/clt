847757a07c39327084f0caa88b5584d4bf418c45
Christopher Hinstorff <chris@chinstorff.com>
2014-07-04 14:24:47 +0200

    add removing and adding queens upon click

--- src/play.js
+++ src/play.js
@@ -2,12 +2,16 @@ Game.Play = function (game) { };
 
 Game.Play.prototype = {
     create: function () {
+	game.input.onDown.add(this.click, this);
+
 	A.grid = new Grid();
 	A.grid.addPossible('queen');
 	A.grid.addPossible('filled');
 
 	game.stage.backgroundColor = '#acf';
 	game.add.sprite(0, 0, 'grid');
+
+	A.pieces = game.add.group();
 
 	A.queens.push([1, 1]);
 	this.updateGrid();
@@ -16,7 +20,6 @@ Game.Play.prototype = {
 
     update: function () {
 
-	this.paint();
     },
 
     updateGrid: function () {
@@ -33,11 +36,14 @@ Game.Play.prototype = {
 
     paint: function () {
 	var value;
+
+	A.pieces.removeAll(true);
+
 	for (var j = 0; j < A.grid.rows; j++) {
 	    for (var i = 0; i < A.grid.columns; i++) {
 		value = A.grid.contentsOf(i, j);
 		if (value) {
-		    game.add.sprite(this.gridToPixels(i), this.gridToPixels(j), value);
+		    A.pieces.create(this.gridToPixels(i), this.gridToPixels(j), value);
 		}
 	    }
 	}
@@ -58,7 +64,45 @@ Game.Play.prototype = {
 
     },
 
+    click: function () {
+	var x = this.pixelsToGrid(game.input.mousePointer.x);
+	var y = this.pixelsToGrid(game.input.mousePointer.y);
+
+	this.updateGrid();
+
+	if (A.grid.contentsOf(x, y) === 'queen') {
+	    this.removeQueen(x, y);
+	}
+	else {
+	    this.addQueen(x, y);
+	}
+
+	this.updateGrid();
+	this.paint();
+    },
+
+    addQueen: function (x, y) {
+	A.queens.push([x, y]);
+	return this;
+    },
+
+    removeQueen: function (x, y) {
+	for (var i = 0; i < A.queens.length; i++) {
+	    var q = A.queens[i];
+	    if (q[0] === x && q[1] === y) {
+		A.queens.splice(i, 1);
+	    }
+	}
+	return this;
+    },
+
     gridToPixels: function (num) {
 	return 3 + 37 * num;
+    },
+
+    pixelsToGrid: function (num) {
+	ret = Math.floor((num - 3) / 37);
+
+	return ret === -1 ? 0 : ret;
     },
 };
